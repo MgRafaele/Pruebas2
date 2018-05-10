799f48674bc9a5e7fccd5b7ad7eaee341e72e464
rmunoz <DAM 6J@A019-PC13>
2018-05-10 11:57:13 +0200

    commit Prueba

---
+++ src/pruebagithub2/Prueba.java
@@ -0,0 +1,29 @@
+
+package pruebagithub2;
+
+/*
+* Prueba.java
+*
+* Creada el 10-may-2018, 11:56:11
+*
+* Desarrollada por Rafael en la empresa Aula6J el día 10-may-2018
+*
+* Puede contactar conmigo en mail callefalsa123@yahoo.com
+
+/*
+
+/**
+ *
+ * @author Rafael
+ * @version 1.01v
+ * @date 10-may-2018
+ */
+public class Prueba {
+    private int numero;
+
+    public Prueba() {
+        numero = 2;
+    }
+    
+    
+}
